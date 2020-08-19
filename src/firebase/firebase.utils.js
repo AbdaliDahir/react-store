11f036d12a7ef675df101f48f993c165ff8b9726
AbdaliDahir <abdali.dahir@gmail.com>
2020-08-19 11:52:57 +0200

    add firebase sign in

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const config = {
+  piKey: "AIzaSyA3jPgKhuuGry7Cyi6swv2rxppk5w3XooM",
+    authDomain: "master-react-store.firebaseapp.com",
+    databaseURL: "https://master-react-store.firebaseio.com",
+    projectId: "master-react-store",
+    storageBucket: "master-react-store.appspot.com",
+    messagingSenderId: "935644620902",
+    appId: "1:935644620902:web:0515a073728bc1a10f65a7",
+    measurementId: "G-V0S66SF4WN"
+}
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
