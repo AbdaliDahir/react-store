5e52abc0188edff25e4c98df12794343b69321b4
abdaliDahir <abdali.dahir@gmail.com>
2020-09-08 16:17:20 +0100

    clear Cart Move Into Saga

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,9 +1,10 @@
 import {all, call} from 'redux-saga/effects';
 
-import { fetchCollectionsStart } from './collections/collection.sagas';
+import { shopSagas } from './collections/collection.sagas';
 import { userSagas } from './user/user.sagas';
+import { cartSagas } from './cart/cart.sagas';
 
 export default function* rootSaga() {
-  yield all([call(fetchCollectionsStart), call(userSagas)])
+  yield all([call(shopSagas), call(userSagas), call(cartSagas)])
 
 }
